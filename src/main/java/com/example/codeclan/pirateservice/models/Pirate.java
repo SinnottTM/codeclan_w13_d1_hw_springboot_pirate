3f0f640eb1b4fb156245176d5971469e5a8096ae
SinnottTM <tim.sinnott86@gmail.com>
2021-05-17 16:59:07 +0100

    3rd commit - Raid & Pirate many to many relationship created

--- src/main/java/com/example/codeclan/pirateservice/models/Pirate.java
+++ src/main/java/com/example/codeclan/pirateservice/models/Pirate.java
@@ -1,5 +1,7 @@
 package com.example.codeclan.pirateservice.models;
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 // Entity allows the program to know we need to use with Javax.
 @Entity
@@ -31,11 +33,28 @@ public class Pirate {
     @JoinColumn(name="ship_id", nullable=false)
     private Ship ship;
 
+    @ManyToMany
+    @JoinTable(
+            name = "pirates_raids",
+            joinColumns = { @JoinColumn(
+                    name = "pirate_id",
+                    nullable = false,
+                    updatable = false)
+            },
+            inverseJoinColumns = { @JoinColumn(
+                    name = "raid_id",
+                    nullable = false,
+                    updatable = false)
+            }
+    )
+    private List<Raid> raids;
+
     public Pirate(String firstName, String lastName, int age, Ship ship) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         this.ship = ship;
+        this.raids = new ArrayList<>();
     }
 
     public Pirate(){
@@ -80,5 +99,13 @@ public class Pirate {
 
     public void setShip(Ship ship) {
         this.ship = ship;
+    }
+
+    public List<Raid> getRaids() {
+        return raids;
+    }
+
+    public void setRaids(List<Raid> raids) {
+        this.raids = raids;
     }
 }
--- src/main/java/com/example/codeclan/pirateservice/models/Raid.java
+++ src/main/java/com/example/codeclan/pirateservice/models/Raid.java
@@ -1,7 +1,5 @@
 package com.example.codeclan.pirateservice.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,8 +19,19 @@ public class Raid {
     @Column(name="loot")
     private int loot;
 
-//    @JsonIgnoreProperties({"raid"})
-    @OneToMany(mappedBy="ship")
+    @ManyToMany
+    @JoinTable(
+            name = "pirates_raids",
+            joinColumns = { @JoinColumn(
+                    name = "raid_id",
+                    nullable = false,
+                    updatable = false)
+            },
+            inverseJoinColumns = { @JoinColumn(
+                    name = "pirate_id",
+                    nullable = false,
+                    updatable = false)
+            })
     private List<Pirate> pirates;
 
     public Raid(String locations, int loot) {
